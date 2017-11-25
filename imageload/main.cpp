eeb9430b9cac2c3e7e8a8c2a290a2a7de2020178
Jiang Chenneng <chenneng.jiang@desay-svautomotive.com>
2017-11-24 23:12:55 -0800

    OpenCV:submit add iamge load code

---
+++ imageload/main.cpp
@@ -0,0 +1,219 @@
+//-----------------------------------【程序说明】----------------------------------------------  
+//  程序名称:：【OpenCV入门教程之三】图像的载入，显示与输出 一站式完全解析 博文配套源码  
+// VS2010版   OpenCV版本：2.4.8  
+//      2014年3月5日 Create by 浅墨  
+//  描述： 图像的载入，显示与输出 一站式剖析   配套源码  
+//  图片素材出处：dota2原画圣堂刺客 dota2 logo  动漫人物  
+//------------------------------------------------------------------------------------------------  
+   
+   
+#include<opencv2/core/core.hpp>  
+#include<opencv2/highgui/highgui.hpp>  
+#include<unistd.h>  
+#include<iostream>
+#include<string>
+ 
+using namespace cv; 
+using namespace std; 
+void Help(string param[], int size);
+void LoadImage();
+bool ROI_AddImage();
+bool LinearBlending();
+bool ROI_LinearBlending();
+
+string all_command[] = {
+	"End Test",
+	"Show Help",
+	"Load Image",
+	"ROI_AddImage",
+	"LinearBlending",
+	"ROI_LinearBlending"
+};
+
+void Help(string param[], int size)
+{
+	printf("select :\n");
+	for(int i = 0; i < size; i++)
+	{
+		printf("%d-%s\n", i, param[i].c_str());
+	}
+}
+
+int main( )  
+{  
+	int select = 1;
+	while((select < sizeof(all_command)/sizeof(all_command[0])) && select != 0)
+	{
+		Help(all_command, sizeof(all_command)/sizeof(all_command[0]));
+		cin >> select;
+		printf("%s\n", all_command[select].c_str());
+
+		if(all_command[select] == "Load Image")
+		{
+			LoadImage();
+		}
+		else if (all_command[select] == "ROI_AddImage")
+		{
+			ROI_AddImage();
+		}
+		else if (all_command[select] == "LinearBlending")
+		{
+			LinearBlending();
+		}
+		else if (all_command[select] == "ROI_LinearBlending")
+		{
+			ROI_LinearBlending();
+		}
+		waitKey(0);  
+   	}
+	return 0;  
+}  
+
+//----------------------------------【LoadImage( )函数】----------------------------------  
+// 函数名：LoadImage（）  
+//     描述：图像加载 
+//----------------------------------------------------------------------------------------------  
+void LoadImage()
+{
+	//-----------------------------------【一、图像的载入和显示】--------------------------------------  
+	//     描述：以下三行代码用于完成图像的载入和显示  
+	//--------------------------------------------------------------------------------------------------  
+	   
+	Mat girl=imread("/work/cv/basesample/CVSample/imageload/image/girl.jpg"); //载入图像到Mat  
+	namedWindow("【1】动漫图"); //创建一个名为 "【1】动漫图"的窗口   
+	imshow("【1】动漫图",girl);//显示名为 "【1】动漫图"的窗口   
+	   
+	//-----------------------------------【二、初级图像混合】--------------------------------------  
+	//     描述：二、初级图像混合  
+	//-----------------------------------------------------------------------------------------------  
+	//载入图片  
+	Mat image= imread("/work/cv/basesample/CVSample/imageload/image/dota.jpg",199);  
+	Mat logo= imread("/work/cv/basesample/CVSample/imageload/image/dota_logo.jpg");  
+	   
+	//载入后先显示  
+	namedWindow("【2】原画图");  
+	imshow("【2】原画图",image);  
+	   
+	namedWindow("【3】logo图");  
+	imshow("【3】logo图",logo);  
+	 /*
+	//定义一个Mat类型，用于存放，图像的ROI  
+	Mat imageROI;  
+	//方法一  
+	//imageROI=image(Rect(800,350,logo.cols,logo.rows));  
+	//方法二  
+	imageROI=image(Range(350,350+logo.rows),Range(800,800+logo.cols));  
+	   
+	//将logo加到原图上  
+	addWeighted(imageROI,0.5,logo,0.3,0.,imageROI);  
+	   
+	//显示结果  
+	namedWindow("【4】原画+logo图");  
+	imshow("【4】原画+logo图",image);  
+	   
+	//-----------------------------------【三、图像的输出】--------------------------------------  
+	//     描述：将一个Mat图像输出到图像文件  
+	//-----------------------------------------------------------------------------------------------  
+	//输出一张jpg图片到工程目录下  
+	imwrite("我喜欢打dota2 by浅墨.jpg",image);  
+	*/ 
+	//waitKey(); 
+
+}
+
+
+//----------------------------------【ROI_AddImage( )函数】----------------------------------  
+// 函数名：ROI_AddImage（）  
+//     描述：利用感兴趣区域ROI实现图像叠加  
+//----------------------------------------------------------------------------------------------  
+bool ROI_AddImage()  
+{  
+   
+       //【1】读入图像  
+       Mat srcImage1= imread("/work/cv/basesample/CVSample/imageload/image/dota.jpg");  
+       Mat logoImage= imread("/work/cv/basesample/CVSample/imageload/image/dota_logo.jpg");  
+       if(!srcImage1.data ) { printf("你妹，读取srcImage1错误~！ \n"); return false; }  
+       if(!logoImage.data ) { printf("你妹，读取logoImage错误~！ \n"); return false; }  
+   
+       //【2】定义一个Mat类型并给其设定ROI区域  
+       Mat imageROI= srcImage1(Rect(200,250,logoImage.cols,logoImage.rows));  
+   
+       //【3】加载掩模（必须是灰度图）  
+       Mat mask= imread("/work/cv/basesample/CVSample/imageload/image/dota_logo.jpg",0);  
+   
+       //【4】将掩膜拷贝到ROI  
+       logoImage.copyTo(imageROI,mask);  
+   
+       //【5】显示结果  
+       namedWindow("<1>利用ROI实现图像叠加示例窗口");  
+       imshow("<1>利用ROI实现图像叠加示例窗口",srcImage1);  
+       //waitKey();
+       return true;  
+}  
+
+
+//---------------------------------【LinearBlending（）函数】-------------------------------------  
+// 函数名：LinearBlending（）  
+// 描述：利用cv::addWeighted（）函数实现图像线性混合  
+//--------------------------------------------------------------------------------------------  
+bool LinearBlending()  
+{  
+       //【0】定义一些局部变量  
+       double alphaValue = 0.5;  
+       double betaValue;  
+       Mat srcImage2, srcImage3, dstImage;  
+   
+       //【1】读取图像 ( 两幅图片需为同样的类型和尺寸 )  
+       srcImage2= imread("/work/cv/basesample/CVSample/imageload/image/mogu.jpg");  
+       srcImage3= imread("/work/cv/basesample/CVSample/imageload/image/rain.jpg");  
+   
+       if(!srcImage2.data ) { printf("你妹，读取srcImage2错误~！ \n"); return false; }  
+       if(!srcImage3.data ) { printf("你妹，读取srcImage3错误~！ \n"); return false; }  
+   
+       //【2】做图像混合加权操作  
+       betaValue= ( 1.0 - alphaValue );  
+       addWeighted(srcImage2, alphaValue, srcImage3, betaValue, 0.0, dstImage);  
+   
+       //【3】创建并显示原图窗口  
+       namedWindow("<2>线性混合示例窗口【原图】 by浅墨", 1);  
+       imshow("<2>线性混合示例窗口【原图】 by浅墨", srcImage2 );  
+   
+       namedWindow("<3>线性混合示例窗口【效果图】 by浅墨", 1);  
+       imshow("<3>线性混合示例窗口【效果图】 by浅墨", dstImage );  
+   	//waitKey(); 
+       return true;  
+        
+}  
+
+
+//---------------------------------【ROI_LinearBlending（）】-------------------------------------  
+// 函数名：ROI_LinearBlending（）  
+// 描述：线性混合实现函数,指定区域线性图像混合.利用cv::addWeighted（）函数结合定义  
+//                     感兴趣区域ROI，实现自定义区域的线性混合  
+//--------------------------------------------------------------------------------------------  
+bool ROI_LinearBlending()  
+{  
+   
+       //【1】读取图像  
+       Mat srcImage4= imread("/work/cv/basesample/CVSample/imageload/image/dota.jpg",1);  
+       Mat logoImage= imread("/work/cv/basesample/CVSample/imageload/image/dota_logo.jpg");  
+   
+       if(!srcImage4.data ) { printf("你妹，读取srcImage4错误~！ \n"); return false; }  
+       if(!logoImage.data ) { printf("你妹，读取logoImage错误~！ \n"); return false; }  
+   
+       //【2】定义一个Mat类型并给其设定ROI区域  
+       Mat imageROI;  
+              //方法一  
+       imageROI=srcImage4(Rect(200,250,logoImage.cols,logoImage.rows));  
+       //方法二  
+       //imageROI=srcImage4(Range(250,250+logoImage.rows),Range(200,200+logoImage.cols));  
+   
+       //【3】将logo加到原图上  
+       addWeighted(imageROI,0.5,logoImage,0.3,0.,imageROI);  
+   
+       //【4】显示结果  
+       namedWindow("<4>区域线性图像混合示例窗口 by浅墨");  
+       imshow("<4>区域线性图像混合示例窗口 by浅墨",srcImage4);  
+        
+       return true;  
+} 
