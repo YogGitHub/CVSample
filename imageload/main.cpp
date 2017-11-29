b2ee82804581b3eb68732625eb6e94f21bf415fa
Jiang Chenneng <chenneng.jiang@desay-svautomotive.com>
2017-11-29 01:17:53 -0800

    OpenCV:submit add contrast and bright implement code

--- imageload/main.cpp
+++ imageload/main.cpp
@@ -23,7 +23,7 @@ bool LinearBlending();
 bool ROI_LinearBlending();
 bool MultiChannelBlending();
 bool Track_bar_display();
-
+bool SetImageContrastAndBright();
 string all_command[] = {
 	"End Test",
 	"Show Help",
@@ -32,7 +32,8 @@ string all_command[] = {
 	"LinearBlending",
 	"ROI_LinearBlending",
 	"MultiChannelBlending",
-	"Track_bar_display"
+	"Track_bar_display",
+	"SetImageContrastAndBright"
 };
 
 void Help(string param[], int size)
@@ -79,6 +80,10 @@ int main( )
 		else if (all_command[select] == "Track_bar_display")
 		{
 			Track_bar_display();
+		}
+		else if (all_command[select] == "SetImageContrastAndBright")
+		{
+			SetImageContrastAndBright();
 		}
 		waitKey(0);  
    	}
@@ -355,7 +360,7 @@ static void on_trackbar(int, void*)
     //定义点和向量  
     vector<vector<Point> > contours;  
     vector<Vec4i> hierarchy;  
-  
+	printf("on_trackbar\n");
     //查找轮廓  
     findContours( bw, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE );  
     //初始化dst  
@@ -382,7 +387,7 @@ bool Track_bar_display()
     //载入图片  
     img = imread("/work/cv/basesample/CVSample/imageload/image/girl2.jpg", 0);  
     if( !img.data ) { printf("Oh，no，读取img图片文件错误~！ \n"); return -1; }  
-  
+  	printf("Track_bar_display\n");
     //显示原图  
     namedWindow( "Image", 1 );  
     imshow( "Image", img );  
@@ -394,3 +399,80 @@ bool Track_bar_display()
     on_trackbar(threshval, 0);//轨迹条回调函数  
 	return 0;
 }
+
+//-----------------------------------【全局函数声明部分】--------------------------------------  
+//     描述：全局函数声明  
+//-----------------------------------------------------------------------------------------------  
+static void ContrastAndBright(int, void *);  
+   
+//-----------------------------------【全局变量声明部分】--------------------------------------  
+//     描述：全局变量声明  
+//-----------------------------------------------------------------------------------------------  
+int g_nContrastValue; //对比度值  
+int g_nBrightValue;  //亮度值  
+Mat g_srcImage,g_dstImage;  
+//-----------------------------------【main( )函数】--------------------------------------------  
+//     描述：控制台应用程序的入口函数，我们的程序从这里开始  
+//-----------------------------------------------------------------------------------------------  
+
+bool SetImageContrastAndBright()
+{
+	   //改变控制台前景色和背景色  
+	system("color5F");   
+  	printf("SetImageContrastAndBright\n");
+	//读入用户提供的图像  
+	g_srcImage= imread( "/work/cv/basesample/CVSample/imageload/image/pic1.jpg");  
+		  if(!g_srcImage.data ) { printf("Oh，no，读取g_srcImage图片错误~！\n"); return false; }  
+	g_dstImage= Mat::zeros( g_srcImage.size(), g_srcImage.type() );  
+
+	//设定对比度和亮度的初值  
+	g_nContrastValue=80;  
+	g_nBrightValue=80;  
+
+	//创建窗口  
+	namedWindow("【效果图窗口】", 1);  
+
+	//创建轨迹条  
+	createTrackbar("对比度：", "【效果图窗口】",&g_nContrastValue,300,ContrastAndBright );  
+	createTrackbar("亮   度：","【效果图窗口】",&g_nBrightValue,200,ContrastAndBright );  
+
+	//调用回调函数  
+	ContrastAndBright(g_nContrastValue,0);  
+	ContrastAndBright(g_nBrightValue,0);  
+
+	//输出一些帮助信息  
+	cout<<endl<<"\t嗯。好了，请调整滚动条观察图像效果~\n\n"  
+				 <<"\t按下“q”键时，程序退出~!\n"  
+				 <<"\n\n\t\t\t\tby浅墨";  
+
+	//按下“q”键时，程序退出  
+	while(char(waitKey(1)) != 'q') {}  
+	   return 0;  
+	return 0;
+}
+
+//-----------------------------【ContrastAndBright( )函数】------------------------------------  
+//     描述：改变图像对比度和亮度值的回调函数  
+//-----------------------------------------------------------------------------------------------  
+static void ContrastAndBright(int, void *)  
+{  
+   
+       //创建窗口  
+       namedWindow("【原始图窗口】", 1);  
+	   printf("ContrastAndBright\n");
+       //三个for循环，执行运算 g_dstImage(i,j) =a*g_srcImage(i,j) + b  
+       for(int y = 0; y < g_srcImage.rows; y++ )  
+       {  
+              for(int x = 0; x < g_srcImage.cols; x++ )  
+              {  
+                     for(int c = 0; c < 3; c++ )  
+                     {  
+                            g_dstImage.at<Vec3b>(y,x)[c]= saturate_cast<uchar>( (g_nContrastValue*0.01)*(g_srcImage.at<Vec3b>(y,x)[c] ) + g_nBrightValue );  
+                     }  
+              }  
+       }  
+   
+       //显示图像  
+       imshow("【原始图窗口】", g_srcImage);  
+       imshow("【效果图窗口】", g_dstImage);  
+}  
